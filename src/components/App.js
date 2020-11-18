6bd24a3b4465e0cd9ff652201f2bb3107cd16fd2
sunyesle <sunyesle@gmail.com>
2020-11-18 11:20:11 +0900

    Nweeting

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,24 @@ import { authService } from 'fbase';
 
 function App() {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
-        setIsLoggedIn(true);
+        setUserObj(user);
       } else {
-        setIsLoggedIn(false);
+        setUserObj(null);
       }
       setInit(true);
     });
   }, []);
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : 'Initailizing....'}
+      {init ? (
+        <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
+      ) : (
+        'Initailizing....'
+      )}
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
---
+++ src/components/Nweet.js
@@ -0,0 +1,58 @@
+import { dbService } from 'fbase';
+import React, { useState } from 'react';
+
+const Nweet = ({ nweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newNweet, setNewNweet] = useState(nweetObj.text);
+  const onDelteClick = async () => {
+    const ok = window.confirm('Are you sure you want to delete this nweet?');
+    if (ok) {
+      await dbService.doc(`nweets/${nweetObj.id}`).delete();
+    }
+  };
+  const toggleEditing = () => setEditing((prev) => !prev);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    await dbService.doc(`nweets/${nweetObj.id}`).update({ text: newNweet });
+    setEditing(false);
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewNweet(value);
+  };
+  return (
+    <div>
+      {editing ? (
+        <>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              placeholder="Edit your nweet"
+              value={newNweet}
+              onChange={onChange}
+              required
+            />
+            <input type="submit" value="Update" />
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4>{nweetObj.text}</h4>
+
+          {isOwner && (
+            <>
+              <button onClick={onDelteClick}>delete</button>
+              <button onClick={toggleEditing}>edit</button>
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Nweet;
